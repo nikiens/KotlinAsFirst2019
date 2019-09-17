9fc85b4510b2d5ee8ab4b3f873916a268080c8d6
Dimitri Anikin <dimitri.anikin@inbox.eu>
2019-09-18 00:23:22 +0300

    lesson2: Cancel brickPasses

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -86,5 +86,4 @@ fun circleInside(
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean =
-    a * b <= r * s || b * c <= r * s || a * c <= r * s
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
