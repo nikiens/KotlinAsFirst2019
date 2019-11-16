727f65fecd62e3fcfcf9d9e4d86f68349c88aa49
Dimitri Anikin <dimitri.anikin@inbox.eu>
2019-11-16 23:35:41 +0300

    lesson7: Reimplement paragraph wrapper

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -387,7 +387,7 @@ fun markdownToHtmlSimple(inputName: String, outputName: String) {
     }
     input.close()
     output =
-        StringBuilder(output.replace(Regex("""[^\r\n]*((\r|\n|\r\n)[^\r\n]*)*""")) { "<p>${it.value}</p>" })
+        StringBuilder(output.replace(Regex("""[^\r\n]+((\r|\n|\r\n)[^\r\n]+)*""")) { "<p>${it.value}</p>" })
             .insert(0, "<html><body>")
             .append("</body></html>")
     File(outputName).writeText(output.toString())
