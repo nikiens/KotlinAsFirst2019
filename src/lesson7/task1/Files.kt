c233c05e39d78447d8a92b70f60874eae300d12c
Dimitri Anikin <dimitri.anikin@inbox.eu>
2019-11-24 23:53:28 +0300

    lesson 7: Undo fix

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -636,9 +636,6 @@ fun printDivisionProcess(lhv: Int, rhv: Int, outputName: String) {
                 it.write("-${subtrahends[i + 1]}".padStart(pad))
                 it.newLine()
 
-                if ("-${subtrahends[i + 1]}".length == newMinuend.length)
-                    redundant += 1
-
                 if (i != broughtDown.lastIndex) {
                     it.write(
                         "".padStart(max(subtrahends[i + 1].length + 1, newMinuend.length), '-')
@@ -648,8 +645,7 @@ fun printDivisionProcess(lhv: Int, rhv: Int, outputName: String) {
                 }
 
                 minuend = newMinuend.toInt()
-                pad += newMinuend.length % subtrahends[i + 1].length + 1 - redundant
-                redundant = 0
+                pad += newMinuend.length % subtrahends[i + 1].length + 1
             }
         }
         it.write(
